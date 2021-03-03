1b29923dd9e173a4f56022d0b44ebf71fb78a3bd
bruceisme123 <lifenghero123@163.com>
2021-03-03 17:30:05 +0800

    更新空行判断，去掉写入函数中无用形参col_num_list，增加列宽调整。

--- excel_null_stat.py
+++ excel_null_stat.py
@@ -23,7 +23,7 @@ import sys
 import time
 import os
 
-def read_excel(path):
+def read_excel_xlrd(path):
     # print(path)
     wb = xlrd.open_workbook(path,encoding_override='utf-8')  # 打开原始文件
     # 默认excel文件的第一个sheet为数据表
@@ -41,7 +41,7 @@ def read_excel(path):
         j=0
         for x in col_value:
             # print(x)
-            if not x:
+            if (not x) or x.isspace():
                 j=j+1
         # print(j)
         # 检查工号/姓名字段的数据是否完整
@@ -49,13 +49,13 @@ def read_excel(path):
             print("工号字段的行数与表的最大行数不同，请检查")
         if j!=0 and col_value[0] == "姓名":
             print("姓名字段的行数与表的最大行数不同，请检查")
-        percent=int((j)/nrows*100)
+        percent=int(j/nrows*100)
         # print(percent)
         col_num_list.append(j)
         col_rat_list.append(str(percent)+"%")
 
 # 简化版：每个输入excel分别生成一个输出excel,不更新已有文件，相同目录下生成“原文件名+result.xlsx”文件
-def write_excel_simple(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
+def write_excel_simple(file_path,nrows,biaotou_list,col_rat_list):
     filename = os.path.basename(file_path).split('.')[-2]
     result_name = filename+'result.xlsx'
     dir_path = os.path.dirname(file_path)+'\\'+result_name
@@ -73,7 +73,7 @@ def write_excel_simple(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
     workbook.close()
 
 # 更新+sheet版：将当前文件夹只存在一个stat_result.xlsx文件，每个sheet对应本文件夹的每个输入excel；stat_result.xlsx文件可更新
-def write_excel(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
+def write_excel(file_path,nrows,biaotou_list,col_rat_list):
     filename = os.path.basename(file_path).split('.')[-2]
     dir_path = os.path.dirname(file_path)+r'\stat_result.xlsx'
     # 如果excel文件已存在则更新或新建sheet，如果文件不存在则新建excel文件
@@ -103,8 +103,8 @@ def write_excel(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
     else:
         workbook = xlsxwriter.Workbook(dir_path)
         addsheet = workbook.add_worksheet(filename)
-        addsheet.merge_range('A1:N1')
         head_str = filename + "（记录条数：" + str(nrows) + "，提交人：            ，接收人：          ）"
+        addsheet.merge_range('A1:N1', head_str)
         addsheet.write('A1', head_str)
         addsheet.write('A2', '字段名称')
         addsheet.write_row('B2', biaotou_list)
@@ -112,10 +112,9 @@ def write_excel(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
         addsheet.write_row('B3', col_rat_list)
         addsheet.write('A4', '数据说明：')
         workbook.close()
-        # sheet.column_dimensions['C'].width = 30
 
 # 更新+sheet+格式版：在write_excel()基础上增加了输出格式的调整
-def write_excel_format(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
+def write_excel_format(file_path,nrows,biaotou_list,col_rat_list):
     filename = os.path.basename(file_path).split('.')[-2]
     dir_path = os.path.dirname(file_path)+r'\stat_result.xlsx'
     # 如果excel文件已存在则更新或新建sheet，如果文件不存在则新建excel文件
@@ -153,6 +152,9 @@ def write_excel_format(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
         ws['A4'] = "数据说明："
         if 'stat_result' in wb:
             del wb['stat_result']
+        # 设置合适的列宽：openpyxl的行或列的编号是从1开始的,openpyxl会根据字符数判断列宽，一个汉字大约等于2个字符,为防止文字紧贴边框，最后加1.5字符宽度
+        for i in range(2,len(biaotou_list)+2):
+            ws.column_dimensions[openpyxl.utils.cell.get_column_letter(i)].width = 2*len(biaotou_list[i-2])+1.5
         wb.save(dir_path)
     else:
         workbook = xlsxwriter.Workbook(dir_path)
@@ -163,18 +165,26 @@ def write_excel_format(file_path,nrows,biaotou_list,col_num_list,col_rat_list):
         addsheet.merge_range('A1:N1', head_str,merge_format)
         # 为统计数据行设置边框
         border_format = workbook.add_format({'border': 1})
-        addsheet.write('A2', '字段名称')
-        addsheet.write_row('B2', biaotou_list)
-        addsheet.write('A3', '空值率')
-        addsheet.write_row('B3', col_rat_list)
+        addsheet.write('A2', '字段名称', border_format)
+        addsheet.write_row('B2', biaotou_list, border_format)
+        addsheet.write('A3', '空值率', border_format)
+        addsheet.write_row('B3', col_rat_list, border_format)
         addsheet.write('A4', '数据说明：')
+        ############################################################################
+        # 使用set_row()设置整行格式，会将无数据的部单元格也设置成目标格式
         # addsheet.set_row(2, None, border_format)
+        ############################################################################
+        # conditional_format为'type':'cell'后，缺少criteria总是报错
         # 根据表头长度计算出数据范围并转化为excel的列表示
-        x = xlsxwriter.utility.xl_col_to_name(len(biaotou_list))
-        pos = 'A2:'+ x +'3'
-        addsheet.conditional_format(pos, {'type':'cell','format': border_format})
+        # x = xlsxwriter.utility.xl_col_to_name(len(biaotou_list))
+        # pos = 'A2:'+ x +'3'
+        # addsheet.conditional_format(pos, {'type':'cell','format': border_format})
+        ############################################################################
+        # 设置合适的列宽
+        for i in range(1, len(biaotou_list) + 1):
+            # 人工测试：11号宋体合适的列宽=1.875*汉字数+0.88
+            addsheet.set_column(i, i, 1.875 * len(biaotou_list[i - 1]) + 0.88)
         workbook.close()
-        # sheet.column_dimensions['C'].width = 30
 
 if __name__ == '__main__':
     start = time.perf_counter()
@@ -192,18 +202,20 @@ if __name__ == '__main__':
                 biaotou_list = []
                 col_num_list = []
                 col_rat_list = []
-                read_excel(file_path)
-                write_excel_simple(file_path, nrows, biaotou_list, col_num_list, col_rat_list)
-                # write_excel(file_path, nrows, biaotou_list, col_num_list, col_rat_list)
-                # write_excel_format(file_path, nrows, biaotou_list, col_num_list, col_rat_list)
+                read_excel_xlrd(file_path)
+                # write_excel_simple(file_path, nrows, biaotou_list, col_rat_list)
+                # write_excel(file_path, nrows, biaotou_list, col_rat_list)
+                write_excel_format(file_path, nrows, biaotou_list, col_rat_list)
                 print('总行数为' + str(nrows))
                 print('各字段缺失情况：')
                 print(biaotou_list)
                 print(col_num_list)
                 print(col_rat_list)
     else:
-        read_excel(path)
-        write_excel(path, nrows, biaotou_list, col_num_list, col_rat_list)
+        read_excel_xlrd(path)
+        # write_excel_simple(path, nrows, biaotou_list, col_rat_list)
+        # write_excel(path, nrows, biaotou_list, col_rat_list)
+        write_excel_format(path, nrows, biaotou_list, col_rat_list)
         print('总行数为' + str(nrows))
         print('各字段缺失情况：')
         print(biaotou_list)
